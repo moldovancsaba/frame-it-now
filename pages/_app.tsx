6dfeaffe280744c47151d19b51a5aa2e28d54c88
Moldovan Csaba Zoltan <moldovancsaba@gmail.com>
2025-07-15 18:31:43 +0200

    fix: Remove reference to deleted camera.css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import '../styles/globals.css';
-import '../styles/camera.css';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps): React.ReactElement {
